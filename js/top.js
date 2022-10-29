5284ed5fc4d8a685670b67b0d8d30a88e6413b76
haruharu3220 <hayato.314.nico@gmail.com>
2022-10-30 01:24:27 +0900

    クリック時に音がなるようにした

--- js/top.js
+++ js/top.js
@@ -1,5 +1,17 @@
 $(".yes").on("click",function(){
 
-    alert("Hello world");
+    //https://maou.audio/
+    $("#yes_audio").get(0).play();
+    setTimeout(function(){
+        window.location.href = "game.html";
+        }, 700);
+});
 
+
+$(".no").on("click",function(){
+
+    $("#yes_audio").get(0).play();
+    setTimeout(function(){
+    window.location.href = "game.html";
+    }, 700);
 });
