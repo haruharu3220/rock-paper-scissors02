5dd8f2348f8bd68eeef3ad844decf069f8e8e9ca
haruharu3220 <hayato.314.nico@gmail.com>
2022-11-02 01:30:21 +0900

    TOPページ作成２

--- js/top.js
+++ js/top.js
@@ -1,25 +1,6 @@
-// $(".start").on("click",function(){
-
-//     //https://maou.audio/
-//     $("#yes_audio").get(0).play();
-//     setTimeout(function(){
-//         window.location.href = "game.html";
-//         }, 700);
-// });
-
-
-// $(".no").on("click",function(){
-
-//     $("#yes_audio").get(0).play();
-//     setTimeout(function(){
-//     window.location.href = "game.html";
-//     }, 700);
-// });
-
 var selecter = 0;
 
 addEventListener('keypress', select);
-
 
 
 function select(e) {
