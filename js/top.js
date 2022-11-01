b0f5c98acc817a92c94f49a3cf7cf5018a172985
haruharu3220 <hayato.314.nico@gmail.com>
2022-11-01 21:32:25 +0900

    キーボード入力で　選択肢の色が変わる

--- js/top.js
+++ js/top.js
@@ -1,17 +1,110 @@
-$(".yes").on("click",function(){
+// $(".start").on("click",function(){
 
-    //https://maou.audio/
-    $("#yes_audio").get(0).play();
-    setTimeout(function(){
-        window.location.href = "game.html";
-        }, 700);
-});
+//     //https://maou.audio/
+//     $("#yes_audio").get(0).play();
+//     setTimeout(function(){
+//         window.location.href = "game.html";
+//         }, 700);
+// });
 
 
-$(".no").on("click",function(){
+// $(".no").on("click",function(){
 
-    $("#yes_audio").get(0).play();
-    setTimeout(function(){
-    window.location.href = "game.html";
-    }, 700);
-});
+//     $("#yes_audio").get(0).play();
+//     setTimeout(function(){
+//     window.location.href = "game.html";
+//     }, 700);
+// });
+
+var selecter = 0;
+
+addEventListener('keypress', select);
+
+function select(e) {
+  	if (e.keyCode === 13) {
+		
+        selecter++;  
+        console.log(selecter);
+        if(selecter===1){
+            $("#start a").css("background","red");
+            $("#select a").css("background","gray");
+            $("#setting a").css("background","gray");
+        }
+
+        if(selecter===2){
+            $("#start a").css("background","gray");
+            $("#select a").css("background","red");
+            $("#setting a").css("background","gray");
+        }
+
+        if(selecter===3){
+            $("#start a").css("background","gray");
+            $("#select a").css("background","gray");
+            $("#setting a").css("background","red");
+        }
+
+        
+	}  
+		return false;
+}
+
+
+function plus(e) {
+    if (e.keyCode === 13) {
+      
+      selecter++;  
+      console.log(selecter);
+      if(selecter===1){
+          $("#start a").css("background","red");
+          $("#select a").css("background","gray");
+          $("#setting a").css("background","gray");
+      }
+
+      if(selecter===2){
+          $("#start a").css("background","gray");
+          $("#select a").css("background","red");
+          $("#setting a").css("background","gray");
+      }
+
+      if(selecter===3){
+          $("#start a").css("background","gray");
+          $("#select a").css("background","gray");
+          $("#setting a").css("background","red");
+      }
+
+      
+  }  
+      return false;
+}
+
+function minus(e) {
+    if (e.keyCode === 13) {
+      
+      selecter++;  
+      console.log(selecter);
+      if(selecter===1){
+          $("#start a").css("background","red");
+          $("#select a").css("background","gray");
+          $("#setting a").css("background","gray");
+      }
+
+      if(selecter===2){
+          $("#start a").css("background","gray");
+          $("#select a").css("background","red");
+          $("#setting a").css("background","gray");
+      }
+
+      if(selecter===3){
+          $("#start a").css("background","gray");
+          $("#select a").css("background","gray");
+          $("#setting a").css("background","red");
+      }
+
+      
+  }  
+      return false;
+}
+
+
+
+
