f53a7d63c504777c3f4c5c4955fb510c30ecb0b2
haruharu3220 <hayato.314.nico@gmail.com>
2022-11-03 06:31:29 +0900

    残アイテムが0判定（クリア判定）

--- js/map.js
+++ js/map.js
@@ -1,12 +1,12 @@
 ////★★★★★★★★パターン3★★★★★★★★
 //https://original-game.com/introduction-to-javascript-character-move-on-map/
 //あたり判定
-var canvas_map = document.getElementById( 'canvas_map');
+var canvas_map = document.getElementById('canvas_map');
 canvas_map.width = 896;	//canvasの横幅（よこはば）
 canvas_map.height = 960;	//canvasの縦幅（たてはば）
- 
+
 //コンテキストを取得（しゅとく）
-var ctx_map = canvas_map.getContext( '2d' );
+var ctx_map = canvas_map.getContext('2d');
 
 //パックマンのオブジェクトを作成
 var pacman = new Object();
@@ -17,7 +17,7 @@ pacman.y = 32;
 pacman.move = 0;
 
 //敵(グー)のオブジェクトを作成
-var  enemy_gu= new Object();
+var enemy_gu = new Object();
 enemy_gu.img = new Image();
 enemy_gu.img.src = 'img/blue.png';
 enemy_gu.x = 64;
@@ -61,53 +61,92 @@ key.push = '';
 //マップの作成（さくせい）
 var map = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1, 1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1],
-	[1,-1, 1, 1, 1, 1,-1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1,-1, 1, 1, 1, 1,-1, 1],
-	[1,-1, 1, 0, 0, 1,-1, 1, 0, 0, 0, 1,-1, 1, 1,-1, 1, 0, 0, 0, 1,-1, 1, 0, 0, 1,-1, 1],
-	[1,-1, 1, 1, 1, 1,-1, 1, 1, 1, 1, 1, -1,1, 1,-1, 1, 1, 1, 1, 1,-1, 1, 1, 1, 1,-1, 1],
-	[1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1],
-	[1,-1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1,-1, 1],
-	[1,-1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1,-1, 1],
-	[1,-1,-1,-1,-1,-1,-1, 1, 1,-1,-1,-1,-1, 1, 1,-1,-1,-1,-1, 1, 1,-1,-1,-1,-1,-1,-1, 1],
-	[1, 1, 1, 1, 1, 1,-1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1,-1, 1, 1, 1, 1, 1, 1],
-	[0, 0, 0, 0, 0, 1,-1, 1, 1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1, 1,-1, 1, 0, 0, 0, 0, 0],
-	[1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1],
-	[0, 0, 0, 0, 0, 0,-1,-1,-1,-1, 1, 0, 0, 0, 0, 0, 0, 1,-1,-1,-1,-1, 0, 0, 0, 0, 0, 0],
-	[1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1],
-	[0, 0, 0, 0, 0, 1,-1, 1, 1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1, 1,-1, 1, 0, 0, 0, 0, 0],
-	[1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1],
-	[1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1, 1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1],
-	[1,-1, 1, 1, 1, 1,-1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1,-1, 1, 1, 1, 1,-1, 1],
-	[1,-1,-1,-1, 1, 1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1, 1,-1,-1,-1, 1],
-	[1, 1, 1,-1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1,-1, 1, 1, 1],
-	[1, 1, 1,-1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1,-1, 1, 1, 1],
-	[1,-1,-1,-1,-1,-1,-1, 1, 1,-1,-1,-1,-1, 1, 1,-1,-1,-1,-1, 1, 1,-1,-1,-1,-1,-1,-1, 1],
-	[1,-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1],
-	[1,-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,-1, 1],
-	[1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1, 1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1],
+	[1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1],
+	[1, -1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, -1, 1],
+	[1, -1, 1, 0, 0, 1, -1, 1, 0, 0, 0, 1, -1, 1, 1, -1, 1, 0, 0, 0, 1, -1, 1, 0, 0, 1, -1, 1],
+	[1, -1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, -1, 1],
+	[1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1],
+	[1, -1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1],
+	[1, -1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1],
+	[1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, 1],
+	[1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1],
+	[0, 0, 0, 0, 0, 1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, 1, 0, 0, 0, 0, 0],
+	[1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1],
+	[0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 1, 0, 0, 0, 0, 0, 0, 1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0],
+	[1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1],
+	[0, 0, 0, 0, 0, 1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, 1, 0, 0, 0, 0, 0],
+	[1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1],
+	[1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1],
+	[1, -1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, -1, 1],
+	[1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, 1],
+	[1, 1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, 1],
+	[1, 1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, 1],
+	[1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, 1],
+	[1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1],
+	[1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1],
+	[1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1],
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
+
+let itemCount = 0; //残アイテムの数をカウント
+let itemCountBuff = 0; //残アイテムの数をカウント
+
+for (var y = 0; y < map.length; y++) {
+	for (var x = 0; x < map[y].length; x++) {
+
+		if (map[y][x] === -1 && x < 5 && y < 5) {
+			itemCount++;
+		}
+	}
+}
+console.log("アイテム数は" + itemCount);
+
+let clearFlig = 0;
 //メインループ
 function main() {
 	// //塗（ぬ）りつぶす色を指定（してい）
 	// ctx_map.fillStyle = "rgb( 0, 0, 0 )";
 	// //塗（ぬ）りつぶす
 	// ctx_map.fillRect(0, 0, 640, 640);
+	let remainItemCount = 0;
+	
+	for (var y = 0; y < map.length; y++) {
+		for (var x = 0; x < map[y].length; x++) {
 
-	for (var y=0; y<map.length; y++) {
-		for (var x=0; x<map[y].length; x++) {
-			if ( map[y][x] === 0 ) ctx_map.drawImage( aisle_pacman, 32*x, 32*y);
-			if ( map[y][x] === -1 ) ctx_map.drawImage( point, 32*x, 32*y);
-			if ( map[y][x] === 1 ) ctx_map.drawImage( aisle,  32*x, 32*y );
+			if (map[y][x] === 0) {
+				ctx_map.drawImage(aisle_pacman, 32 * x, 32 * y);
+			}
+			else if (map[y][x] === -1 && x < 5 && y < 5) {
+				ctx_map.drawImage(point, 32 * x, 32 * y);
+				remainItemCount++;
+				//console.log("map[y][x]="+ map[y][x]);
+			}
+			else if (map[y][x] === 1) {
+				ctx_map.drawImage(aisle, 32 * x, 32 * y);
+			}
+			else { //デバッグ用コード　動作確認のためアイテムの数を減らす
+				ctx_map.drawImage(aisle_pacman, 32 * x, 32 * y);
+			}
+		}
+	}
+
+	//残アイテムが0判定（クリア判定）
+	//main 関数が無限ループしているからその中でクリア判定を実施し
+	//クリアなら１回だけ通知を上げるところが難しかった
+	if (remainItemCount === 0 && clearFlig==0) {
+		clearFlig++
+		//console.log("A");
+		if (clearFlig === 1) {
+			console.log("クリア");
 		}
 	}
 
 	//パックマンを表示
-	ctx_map.drawImage( pacman.img, pacman.x, pacman.y ,32,32); //32pxにリサイズ
+	ctx_map.drawImage(pacman.img, pacman.x, pacman.y, 32, 32); //32pxにリサイズ
 
 	//パックマンが一度通った道のアイテムは消す
-	if ( pacman.move === 0 ) {
-	map[pacman.y/32][pacman.x/32] = 0;
+	if (pacman.move === 0) {
+		map[pacman.y / 32][pacman.x / 32] = 0;
 	}
 
 	addEventListener("keydown", keydownfunc02, false);
@@ -116,82 +155,82 @@ function main() {
 	//方向キーが押されている場合は、りこちゃんが移動する
 	//あたり判定
 	//https://original-game.com/introduction-to-javascript-character-move-on-map/
-	if ( pacman.move === 0 ) {
-		if ( key.left === true ) {
-            
-            var x = pacman.x/32;
-            var y = pacman.y/32;
-            x--;
-                if ( map[y][x] != 1 ) {
-                    pacman.move = 32;
-                    key.push = 'left';
-                }
-            }
-		
-		if ( key.up === true ) {
-            var x = pacman.x/32;
-            var y = pacman.y/32;
-            if ( y > 0) {
-                y--;
-                if ( map[y][x] != 1 ) {
-                    pacman.move = 32;
-                    key.push = 'up';
-                }
-            }
+	if (pacman.move === 0) {
+		if (key.left === true) {
+
+			var x = pacman.x / 32;
+			var y = pacman.y / 32;
+			x--;
+			if (map[y][x] != 1) {
+				pacman.move = 32;
+				key.push = 'left';
+			}
+		}
+
+		if (key.up === true) {
+			var x = pacman.x / 32;
+			var y = pacman.y / 32;
+			if (y > 0) {
+				y--;
+				if (map[y][x] != 1) {
+					pacman.move = 32;
+					key.push = 'up';
+				}
+			}
 
 		}
-		if ( key.right === true ) {
-            
-            var x = pacman.x/32;
-            var y = pacman.y/32;
-            x++;
-                if ( map[y][x] != 1 ) {
-                    pacman.move = 32;
-                    key.push = 'right';
-                }
-            }
-		if ( key.down === true ) {
-            var x = pacman.x/32;
-			var y = pacman.y/32;
-			if ( y < 30 ) {
+		if (key.right === true) {
+
+			var x = pacman.x / 32;
+			var y = pacman.y / 32;
+			x++;
+			if (map[y][x] != 1) {
+				pacman.move = 32;
+				key.push = 'right';
+			}
+		}
+		if (key.down === true) {
+			var x = pacman.x / 32;
+			var y = pacman.y / 32;
+			if (y < 30) {
 				y++;
-				if ( map[y][x] != 1 ) {
+				if (map[y][x] != 1) {
 					pacman.move = 32;
 					key.push = 'down';
 				}
 			}
 		}
-    }
+	}
 
 	//rico.moveが0より大きい場合は、4pxずつ移動（いどう）を続ける
 	if (pacman.move > 0) {
 		pacman.move -= 4;
-		if ( key.push === 'left' )  pacman.x -= 4;
-		if ( key.push === 'up' )    pacman.y -= 4;
-		if ( key.push === 'right' ) pacman.x += 4;
-		if ( key.push === 'down' )  pacman.y += 4;
+		if (key.push === 'left') pacman.x -= 4;
+		if (key.push === 'up') pacman.y -= 4;
+		if (key.push === 'right') pacman.x += 4;
+		if (key.push === 'down') pacman.y += 4;
 	}
 
-	requestAnimationFrame( main );
+	requestAnimationFrame(main);
 }
 //ページと依存（いぞん）している全てのデータが読み込まれたら、メインループ開始
 addEventListener('load', main(), false);
 
 //キーボードが押されたときに呼び出される関数（かんすう）
-function keydownfunc02( event ) {
+function keydownfunc02(event) {
 	var key_code = event.keyCode;
-	if( key_code === 37 ) key.left = true;
-	if( key_code === 38 ) key.up = true;
-	if( key_code === 39 ) key.right = true;
-	if( key_code === 40 ) key.down = true;
+	if (key_code === 37) key.left = true;
+	if (key_code === 38) key.up = true;
+	if (key_code === 39) key.right = true;
+	if (key_code === 40) key.down = true;
 	event.preventDefault();
 }
 
 //キーボードが放（はな）されたときに呼び出される関数
-function keyupfunc02( event ) {
+function keyupfunc02(event) {
 	var key_code = event.keyCode;
-	if( key_code === 37 ) key.left = false;
-	if( key_code === 38 ) key.up = false;
-	if( key_code === 39 ) key.right = false;
-	if( key_code === 40 ) key.down = false;
+	if (key_code === 37) key.left = false;
+	if (key_code === 38) key.up = false;
+	if (key_code === 39) key.right = false;
+	if (key_code === 40) key.down = false;
 }
